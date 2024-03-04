11d77eac6c50f90ba7ba325180b42d3122e4eace
deduss <shinji2913@yandex.ru>
2024-03-04 16:29:15 +0300

    добавление ПОО для участников

--- Olimp/Data/Result.cs
+++ Olimp/Data/Result.cs
@@ -9,7 +9,7 @@ public class Result : IEntityId<Guid>
     public Guid Id { get; set; }
     [DisplayName("Номер участника")] public Guid ParticipantId { get; set; }
     [DisplayName("Номер участка")] public Guid StepId { get; set; }
-    public required Participant Participant { get; set; }
-    public required Step Step { get; set; }
+    [DisplayName("Номер участника")] public required Participant Participant { get; set; }
+    [DisplayName("Номер участка")] public required Step Step { get; set; }
     [DisplayName("Результат")] public required decimal Score { get; set; }
 }
